338790decf5ac8bb682a50434ef0a857c11b1190
TRAVOR SSETTUBA <160039863+p1118231@users.noreply.github.com>
2024-12-12 23:26:53 +0000

    Update Program.cs

--- ThamcoProducts/Program.cs
+++ ThamcoProducts/Program.cs
@@ -22,8 +22,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpClient();  // Register HttpClient for DI
 
 builder.Services.AddScoped<IUndercuttersService, UndercuttersService>();
-
-
+builder.Services.AddScoped<IProductService, ProductFakeService>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     .AddJwtBearer(options =>
@@ -36,7 +35,7 @@ builder.Logging.AddConsole();
 
 if(builder.Environment.IsDevelopment()){
    // builder.Services.AddSingleton<IUndercuttersService, UndercuttersFakeService>();
-    builder.Services.AddSingleton<IProductSerivce, ProductFakeService>();
+    builder.Services.AddSingleton<IProductService, ProductFakeService>();
 }
 else {
 
